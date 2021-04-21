996e9985b753b5b1bf82f3749e0ea0c8baa33b39
Xh0st <and.andrei@outlook.com>
2021-04-21 18:13:59 +0300

    Add files via upload

---
+++ blah64.py
@@ -0,0 +1,29 @@
+# Xh0st Keyspace Puzzle 64 Random
+# Made by Andrei Melek
+# https://github.com/xh0st/keyspace64
+
+from bitcoin import *
+import random
+
+
+while True:  
+    low  = 0x8000000000000000 
+    high = 0xffffffffffffffff
+    val = str ( hex ( random.randrange( low, high ) ) )[2:]
+    result = val.rjust(48 + len(val), '0')
+    priv = result
+    pub = privtopub(priv)
+    pubkey1 = encode_pubkey(privtopub(priv), "bin_compressed")
+    addr = pubtoaddr(pubkey1)
+    n = addr
+    if n.startswith('16jY7qLJnxb7C'):
+        print ("found!!",addr,result)
+        s1 = result
+        s2 = addr
+        s3 = pub
+
+        file = open('boom.txt', 'a')
+        file.write("Private key: " + s1 + '\n' + "Public key: " + s3 + '\n' + "Address: " + s2 + '\n\n')    
+        file.close()
+    else:
+        print ("searching...",addr,result)
