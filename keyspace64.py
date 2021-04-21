4947a79fbe59d3420b409c3b5c59cd3ca727e0e0
Xh0st <and.andrei@outlook.com>
2021-04-21 18:19:16 +0300

    Update and rename blah64.py to keyspace64.py

--- keyspace64.py
+++ keyspace64.py
@@ -1,29 +1,34 @@
-# Xh0st Keyspace Puzzle 64 Random
+# Keyspace Puzzle 64 Random
 # Made by Andrei Melek
 # https://github.com/xh0st/keyspace64
 
-from bitcoin import *
-import random
+try:
+    from bitcoin import *
+    import random
 
+# If required imports are unavailable, we will attempt to install them!
+
+except ImportError: 
+    import subprocess
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'bitcoin'])
 
 while True:  
     low  = 0x8000000000000000 
     high = 0xffffffffffffffff
     val = str ( hex ( random.randrange( low, high ) ) )[2:]
-    result = val.rjust(48 + len(val), '0')
-    priv = result
+    priv = val.rjust(48 + len(val), '0')
     pub = privtopub(priv)
     pubkey1 = encode_pubkey(privtopub(priv), "bin_compressed")
     addr = pubtoaddr(pubkey1)
     n = addr
     if n.startswith('16jY7qLJnxb7C'):
         print ("found!!",addr,result)
-        s1 = result
-        s2 = addr
-        s3 = pub
+        k1 = priv
+        k2 = pub
+        k3 = addr
 
         file = open('boom.txt', 'a')
-        file.write("Private key: " + s1 + '\n' + "Public key: " + s3 + '\n' + "Address: " + s2 + '\n\n')    
+        file.write("Private key: " + k1 + '\n' + "Public key: " + k2 + '\n' + "Address: " + k2 + '\n\n')    
         file.close()
     else:
         print ("searching...",addr,result)
