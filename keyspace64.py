ba96d50dc28736b0c22e0bc1bbba68b4e57e2d74
Xh0st <and.andrei@outlook.com>
2021-04-21 20:18:02 +0300

    Update keyspace64.py

--- keyspace64.py
+++ keyspace64.py
@@ -5,43 +5,31 @@
 try:
     import random
     from bitcoin import *
-    import random
-    import hashlib
-    import binascii
-    import ecdsa
 
 # If required imports are unavailable, we will attempt to install them!
 
 except ImportError: 
     import subprocess
     subprocess.check_call(["python3", '-m', 'pip', 'install', 'bitcoin'])
-    subprocess.check_call(["python3", '-m', 'pip', 'install', 'ecdsa'])
 
 while True:  
     low  = 0x8000000000000000 
     high = 0xffffffffffffffff
     val = str ( hex ( random.randrange( low, high ) ) )[2:]
-    private_key = val.rjust(48 + len(val), '0')
-    sign = ecdsa.SigningKey.from_string(binascii.unhexlify(private_key), curve = ecdsa.SECP256k1)
-    key_bytes = binascii.hexlify(sign.verifying_key.to_string()).decode('utf-8')
-    key = ('0x' + binascii.hexlify(sign.verifying_key.to_string()).decode('utf-8'))
-    # Get X from the key (first half)
-    half_len = len(key_bytes) // 2
-    key_half = key_bytes[:half_len]
-    # Add bitcoin byte: 0x02 if the last digit is even, 0x03 if the last digit is odd
-    last_byte = int(key[-1], 16)
-    bitcoin_byte = '02' if last_byte % 2 == 0 else '03'
-    public_key = bitcoin_byte + key_half
-    addr = pubtoaddr(public_key)
+    result = val.rjust(48 + len(val), '0')
+    priv = result
+    pub = privtopub(priv)
+    pubkey1 = encode_pubkey(privtopub(priv), "bin_compressed")
+    addr = pubtoaddr(pubkey1)
     n = addr
-    if n.startswith('16jY7qLJnx'):
-        print ("found!!",addr,private_key)
-        k1 = private_key
-        k2 = public_key
+    if n.startswith('16jY7qLJnxb'):
+        print ("found!!",addr,result)
+        k1 = priv
+        k2 = pub
         k3 = addr
 
         file = open('boom.txt', 'a')
         file.write("Private key: " + k1 + '\n' + "Public key: " + k2 + '\n' + "Address: " + k3 + '\n\n')    
         file.close()
     else:
-        print ("searching...",addr,private_key)
+        print ("searching...",addr,result)
